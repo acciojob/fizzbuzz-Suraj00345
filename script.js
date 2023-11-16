ce8ac7d930befaca1d69544e6aea47d9728096c5
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-16 18:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function fizzbuzz(n) {
+	let result = [];
+	for(let i=1;i<=n;++i)
+		{
+			if(i%3 == 0 && i%5 == 0)
+			{
+			 result.push_back("FizzBuzz");
+            }
+			else if(i%3 == 0)
+			{
+				result.push_back("Fizz");
+			}
+			else if(i%5 == 0)
+			{
+				result.push_back("Buzz");
+			}
+			else {
+				  result.push_back(to_string(i));
+			}
+		}
+	return result;
+}
+let n = 20;
+ 
+// Call the fizzBuzz function to get the result
+let result = fizzBuzz(n);
+ 
+// Print the result
+console.log(result.join(' '));
